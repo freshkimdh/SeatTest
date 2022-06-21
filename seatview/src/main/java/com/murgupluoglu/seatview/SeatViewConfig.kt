1cc6a3a361b6eb7b0167464654a509e96c317e86
DESKTOP-71VEOOF\Daehwan <freshkimdh@gmail.com>
2022-06-21 17:10:59 +0900

    좌석 row, column 간격 조정

--- seatview/src/main/java/com/murgupluoglu/seatview/SeatViewConfig.kt
+++ seatview/src/main/java/com/murgupluoglu/seatview/SeatViewConfig.kt
@@ -21,8 +21,8 @@ data class SeatViewConfig(
     var seatMaxWidth: Float = 30f.dp2px(),
     var seatDefaultWidth: Float = 20f.dp2px(), // 좌석 width 조정
     var seatWidthHeightRatio: Float = 1f,
-    var seatInlineGapWidthRatio: Float = 0.265f,
-    var seatNewlineGapWidthRatio: Float = 0.304f,
+    var seatInlineGapWidthRatio: Float = 0.265f, // 좌석 column 간격 gap 조정
+    var seatNewlineGapWidthRatio: Float = 0.904f, // 좌석 row 간격 gap 조정
 
     @ColorInt
     var backgroundColor: Int = Color.GRAY,
